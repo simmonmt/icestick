f04706799d07f4dadda3bacf65d6db85780e009a
Matthew Simmons <simmonmt@acm.org>
2019-03-24 16:22:54 -0400

    Add more/better assertions to ball logic

--- 8bit/abs_pong_logic_test.v
+++ 8bit/abs_pong_logic_test.v
@@ -7,9 +7,21 @@ module test;
    initial begin
       #   0 reset = 1;
       #   5 reset = 0;
-      #  35 `assert(ball_hpos, 18);
+      #  95 $stop;
+   end
+
+   initial begin
+      #   5 `assert(ball_hpos, 10);
+      #  25 `assert(ball_hpos, 16);
+      #  10 `assert(ball_hpos, 18);
       #  10 `assert(ball_hpos, 16);
-      # 100 $stop;
+   end
+
+   initial begin
+      #   5 `assert(ball_vpos, 10);
+      #  35 `assert(ball_vpos, 2);
+      #  10 `assert(ball_vpos, 0);
+      #  10 `assert(ball_vpos, 2);
    end
 
    reg clk = 0;
@@ -24,8 +36,8 @@ module test;
 			       .ball_vpos(ball_vpos));
 
    initial
-     $monitor("time %4t / clk %d / hpos %d mv %x c %d / hpos %d mv %x c %d",
-	      $time, clk,
+     $monitor("time %4t / clk %d r %d / hpos %d mv %x c %d / vpos %d mv %x c %d",
+	      $time, clk, reset,
 	      ball_hpos, uut.ball_horiz_move, uut.ball_horiz_collide,
 	      ball_vpos, uut.ball_vert_move, uut.ball_vert_collide);
 
