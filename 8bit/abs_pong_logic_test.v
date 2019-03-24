c40195246b0158db574449af61f26b1167db48eb
Matthew Simmons <simmonmt@acm.org>
2019-03-24 16:14:09 -0400

    Add some assertions to the logic test

--- 8bit/abs_pong_logic_test.v
+++ 8bit/abs_pong_logic_test.v
@@ -5,13 +5,15 @@ module test;
    reg reset = 0;
 
    initial begin
-      # 0 reset = 1;
-      # 5 reset = 0;
+      #   0 reset = 1;
+      #   5 reset = 0;
+      #  35 `assert(ball_hpos, 18);
+      #  10 `assert(ball_hpos, 16);
       # 100 $stop;
    end
 
    reg clk = 0;
-   always #1 clk = !clk;
+   always #5 clk = !clk;
 
    wire [8:0] ball_hpos;
    wire [8:0] ball_vpos;
@@ -22,8 +24,8 @@ module test;
 			       .ball_vpos(ball_vpos));
 
    initial
-     $monitor("time %4t / hpos %d mv %x c %d / hpos %d mv %x c %d",
-	      $time, 
+     $monitor("time %4t / clk %d / hpos %d mv %x c %d / hpos %d mv %x c %d",
+	      $time, clk,
 	      ball_hpos, uut.ball_horiz_move, uut.ball_horiz_collide,
 	      ball_vpos, uut.ball_vert_move, uut.ball_vert_collide);
 
